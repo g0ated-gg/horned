9305e4d8e7c4456952b3ff6c0696c84dc48d11e7
Sergey Skokov <sergskokow@gmail.com>
2025-11-11 13:58:32 +0300

    Camera optimizations

--- prototype2/cameraman.gd
+++ prototype2/cameraman.gd
@@ -12,82 +12,74 @@ class_name Cameraman extends CharacterBody3D
 @onready var floor_raycast: RayCast3D = $FloorRayCast3D
 
 
-@export var camera_sensitivity: float = 0.05
-@export var movement_speed: float = 10.0 # Скорость движения камеры
+@export var camera_sensitivity: float = 0.01
+@export var movement_speed: float = 10.0
 
 var dragging: bool = false
-var drag_start_mouse_position: Vector2 = Vector2.ZERO
+var drag_accum: Vector2 = Vector2.ZERO
 var camera_velocity: Vector3 = Vector3.ZERO
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("camera"):
-		if event is InputEventMouse:
-			dragging = true
-			drag_start_mouse_position = event.position
-	elif event.is_action_released("camera"):
-		dragging = false
-
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+		dragging = event.pressed
 	if event is InputEventMouseMotion and dragging:
-		var delta = event.relative
-		camera_velocity += -transform.basis.x * delta.x * camera_sensitivity * movement_speed
-		camera_velocity += -transform.basis.z * delta.y * camera_sensitivity * movement_speed
+		drag_accum += event.relative
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	if Input.is_action_just_pressed("center"):
 		Global.centering = !Global.centering
 
-	if dragging and not Global.centering:
-		velocity.x = camera_velocity.x
-		velocity.z = camera_velocity.z
-		camera_velocity = Vector3.ZERO
-	if direction and not Global.centering:
-		velocity.x = direction.x * speed.x
-		velocity.z = direction.y * speed.y
-	elif not dragging:
-		velocity.x = move_toward(velocity.x, 0, speed.x)
-		velocity.z = move_toward(velocity.z, 0, speed.y)
-
 	var cursor_position = _get_cursor_global_position()
 	if cursor_position and player:
-		var player_direction = player.global_position.direction_to(cursor_position)
-		player_direction.y = player.global_position.y
-		player.look_direction = player_direction
+		var player_direction = cursor_position - player.global_position
+		player_direction.y = 0.0
+		if player_direction.length() > 0.001:
+			player.look_direction = player_direction.normalized()
 
-	if Global.centering:
+	velocity = Vector3.ZERO
+
+	if Global.centering and player:
 		global_position = _get_centered_position()
-		move_and_slide()
+		if floor_raycast.is_colliding():
+			global_position.y = floor_raycast.get_collision_point().y
+		return
+
+	if dragging and drag_accum != Vector2.ZERO:
+		var move_world = (-transform.basis.x * drag_accum.x - transform.basis.z * drag_accum.y) \
+			* (camera_sensitivity / max(delta, 1e-6))
+		velocity.x = move_world.x
+		velocity.z = move_world.z
+		drag_accum = Vector2.ZERO
+	elif direction != Vector2.ZERO:
+		velocity.x = direction.x * speed.x
+		velocity.z = direction.y * speed.y
+
+	move_and_slide()
+
+	if floor_raycast.is_colliding():
 		global_position.y = floor_raycast.get_collision_point().y
-	else:
-		move_and_slide()
-		if dragging:
-			if floor_raycast.is_colliding():
-				global_position.y = floor_raycast.get_collision_point().y
-			velocity = Vector3.ZERO 
 
 func on_border_reached(border: Vector2) -> void:
-	if not dragging:
+	if dragging:
+		direction = Vector2.ZERO
+	else:
 		direction = border
 
 func _get_centered_position() -> Vector3:
-	return Vector3(
-		player.global_position.x,
-		0.0,
-		player.position.z - height * tan(PI/2 - camera.rotation.x)
-	)
+	var xz_offset := height * tan(PI * 0.5 - camera.rotation.x)
+	return Vector3(player.global_position.x, 0.0, player.global_position.z - xz_offset)
 
-func _get_cursor_global_position(ray_length: float = 1000.0):
-	var mouse_position = get_viewport().get_mouse_position()
-	var ray_origin: Vector3 = camera.project_ray_origin(mouse_position)
-	var ray_direction: Vector3 = camera.project_ray_normal(mouse_position)
-	var ray_end: Vector3 = ray_origin + ray_direction * ray_length
-	var space_state = get_world_3d().direct_space_state
-	var result = space_state.intersect_ray(
-		PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
-	)
-	if result:
-		return result.position
-	else:
-		return null
+func _get_cursor_global_position(ray_length: float = 1000.0) -> Variant:
+	var mouse_pos := get_viewport().get_mouse_position()
+	var ray_origin: Vector3 = camera.project_ray_origin(mouse_pos)
+	var ray_dir: Vector3 = camera.project_ray_normal(mouse_pos)
+	var ray_end: Vector3 = ray_origin + ray_dir * ray_length
+
+	var params := PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	params.collide_with_areas = false
+	params.exclude = [self.get_rid()]
+
+	var hit := get_world_3d().direct_space_state.intersect_ray(params)
+	if hit.has("position"):
+		return hit.position
+	return null
